218df0f5a64938749d0bef62d89d0d87a6cc22fe
Iamqaasim <iamqaasim@gmail.com>
2023-04-29 22:33:57 +0200

    Update downloader.py

--- Beginner/Youtube_video_downloader/downloader.py
+++ Beginner/Youtube_video_downloader/downloader.py
@@ -4,7 +4,7 @@ from pytube import YouTube  # Used to get data from YouTube
 from sys import argv        # Used to take input commands when running python file
 
 # YouTube connection
-link = argv[1] # Note: argv[0] is going to be the file you are running 
+link = argv[2] # Note: argv[0] is going to be python and argv[1] is going to be the file you are running 
 yt = YouTube(link)
 
 # Test Connection
